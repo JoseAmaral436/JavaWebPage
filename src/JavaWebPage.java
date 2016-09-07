1259d222f18767a861576094a78cfd0ac5c4591b
ist176346 <jose_amaral436@hotmail.com>
2016-09-07 23:27:46 +0100

    first commit.

--- src/JavaWebPage.java
+++ src/JavaWebPage.java
@@ -9,23 +9,101 @@ import com.sun.net.httpserver.HttpServer;
 public class JavaWebPage {
 
     public static void main(String[] args) throws Exception {
-        HttpServer server;
-        server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+        JavaWebPage jwp = new JavaWebPage();
+        jwp.handling();
+    }
+
+    public void handling() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/f.html", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
-
-    static class MyHandler implements HttpHandler {
+    class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response;
-            response = "This was the query:" + t.getRequestURI().getQuery() + "##";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            System.out.println("***********REQUEST RECEIVED ###########");
+            try {
+
+                new Thread(new HandleThread(t)).start();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    class HandleThread implements Runnable {
+
+        private HttpExchange t;
+        private String query = "hello";
+
+        public HandleThread(HttpExchange h) throws Exception {
+            this.t = h;
+        }
+
+        public void run() {
+            try {
+                Thread.sleep(3000);
+                this.t.sendResponseHeaders(200, query.length());
+                OutputStream os = this.t.getResponseBody();
+                os.write(query.getBytes());
+                os.close();
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*public void handling() throws IOException {
+        HttpServer server;
+        int nThreads = 5;
+        server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(java.util.concurrent.Executors.newFixedThreadPool(nThreads));
+        server.start();
+    }
+
+    class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            new Thread(new HandleThread(t)).start();
+        }
+    }
+
+    class HandleThread implements Runnable {
+
+        private HttpExchange t;
+
+        public HandleThread(HttpExchange h) {
+            this.t = h;
+        }
+
+        public void run() {
+            try {
+                long threadId = Thread.currentThread().getId();
+                System.out.println(threadId);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                threadId = Thread.currentThread().getId();
+                System.out.println(threadId);
+                String response;
+                OutputStream os;
+                response = "This was a query:";
+                this.t.sendResponseHeaders(200, response.length());
+                os = this.t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }*/
+
 
 }
